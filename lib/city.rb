c5753f4b82045fa200b731cdc856a48bcca7387e
Isaac Elias <isaac@truelinkfinancial.com>
2020-09-12 13:53:08 -0700

    Barclays added

--- lib/city.rb
+++ lib/city.rb
@@ -1,8 +1,9 @@
 class City
 
   CITY_DATA = YAML.load_file 'city_data.yml'
+  BARCLAYS_CAPACITY = 17732
   FIRST_RECORD = 174365
-  attr_reader :name, :state, :population
+  attr_reader :name, :state, :population, :extra_barclays
 
   def initialize(current_covid_deaths)
     # city = CITY_DATA[FIRST_RECORD]
@@ -11,6 +12,16 @@ class City
     @population = city_key
     @name = city['name']
     @state = city['state']
+    @extra_barclays = init_extra_barclays(current_covid_deaths)
+  end
+
+  def init_extra_barclays(deaths)
+    diff = deaths - @population
+    if diff > BARCLAYS_CAPACITY
+      diff / BARCLAYS_CAPACITY
+    else
+      0
+    end
   end
 
 end
